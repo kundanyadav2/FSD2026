3a95f0e68f7c1b8240c79aca8e109bf88e73acc9
Kundan Yadav <kundanyadavcs27@gmail.com>
2026-02-19 12:17:18 +0530

    commit

--- day_04/status.js
+++ day_04/status.js
@@ -1,3 +1,13 @@
 import fs from "fs/promises";
 
-const
+const status = async (path) => {
+    try {
+      const stats = await fs.stat(path);
+      console.log(stats.size);
+      console.log(stats.isFile());
+      console.log(stats.isDirectory());           
+    } catch (error) {
+     console.log("unable to check status");     
+    }
+}
+status("./dir.js")
